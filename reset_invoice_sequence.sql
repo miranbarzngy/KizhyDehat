06dae824bc14adf986b3958b9fbed83c9c8904a1
Developer <dev@example.com>
2026-02-25 12:36:45 +0300

    fix: update SQL to allow sequence minvalue 0

--- reset_invoice_sequence.sql
+++ reset_invoice_sequence.sql
@@ -1,10 +1,13 @@
 -- Reset invoice sequence to start from 0
 -- Run this in Supabase SQL Editor
 
+-- First, alter the sequence to allow 0 as minimum
+ALTER SEQUENCE invoice_num_seq MINVALUE 0;
+
 -- Reset the sequence to start from 0 (so next value will be 1)
 SELECT setval('invoice_num_seq', 0);
 
--- Optionally update the invoice_settings to reflect the change
+-- Update the invoice_settings to reflect the change
 UPDATE invoice_settings 
 SET starting_invoice_number = 1, 
     current_invoice_number = 1
